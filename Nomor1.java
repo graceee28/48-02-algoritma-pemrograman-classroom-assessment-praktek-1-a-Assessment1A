b95b936a2d6516f20de83efd5b331735bc1d7d01
graceee28 <183887439+graceee28@users.noreply.github.com>
2024-10-24 12:46:46 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
 public class Nomor1{
     public static void main(String[] args){
+        
+        int TotalJamlembur;
+        int honorLembur;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumblah jam lembur: ");
+        int jamLembur = scanner.nextInt();
+
+        double honor = 0.0;
+
+        if (jamLembur <= 16) {
+            honor = jamLembur * 10000;
+        } else {
+            honor = 16 * 10000 + (jamLembur - 16) * 15000;
+        }
+
+        System.out.println("Total jam lembur: " + jamLembur);
+        System.out.println("Honor lembur: " + honor);
     }
 }
